3ed76836244af4c75773ae3db249de806050e1c5
Jonas Hansen <jonas.jmh@gmail.com>
2016-02-20 14:13:17 +0100

    Removed some tests

--- Software/RollingRoadGUI/RollingRoad.Test.Unit/SystemTimerTests.cs
+++ Software/RollingRoadGUI/RollingRoad.Test.Unit/SystemTimerTests.cs
@@ -6,41 +6,5 @@ namespace RollingRoad.Test.Unit
     [TestFixture]
     public class SystemTimerTests
     {
-        [Test]
-        public void Start_SingleCallToStart_OneInvoke()
-        {
-            SystemTimer timer = new SystemTimer();
-            int invokes = 0;
-
-            timer.Elapsed += () =>
-            {
-                invokes++;
-            };
-
-            timer.Start(10);
-
-            while (invokes != 1){}
-
-            Assert.That(invokes, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void Start_TwoCallsToStart_TwoInvokes()
-        {
-            SystemTimer timer = new SystemTimer();
-            int invokes = 0;
-
-            timer.Elapsed += () =>
-            {
-                invokes++;
-            };
-
-            timer.Start(5);
-            timer.Start(5);
-
-            while (invokes != 2) { }
-
-            Assert.That(invokes, Is.EqualTo(2));
-        }
     }
 }
