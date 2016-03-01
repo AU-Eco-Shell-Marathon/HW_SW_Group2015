af0191a20a922169b577a84a80921841eccbcd60
ThomasN <komlir@hotmail.com>
2016-03-01 12:54:51 +0100

    still doesnt work

--- Software/RollingRoadUART/test9/test9.cydsn/codegentemp/test9.ctl
+++ Software/RollingRoadUART/test9/test9.cydsn/codegentemp/test9.ctl
@@ -1,6 +1,6 @@
 -- ======================================================================
 -- test9.ctl generated from test9
--- 03/01/2016 at 10:49
+-- 03/01/2016 at 12:54
 -- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 -- ======================================================================
 
--- Software/RollingRoadUART/test9/test9.cydsn/codegentemp/test9.v
+++ Software/RollingRoadUART/test9/test9.cydsn/codegentemp/test9.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // test9.v generated from TopDesign.cysch
-// 03/01/2016 at 10:49
+// 03/01/2016 at 12:54
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
