3040b2f78e1ef1266d2f0c07db28fbaf6a96d021
Thomas Skovgaard <thomasskovgard@gmail.com>
2016-05-26 15:52:52 +0200

    fuck jeg får mange linjer nu

---
+++ Appendix/Software/BatteryManagementSystem/bmsmain21/default/dep/BMSmain1.1.o.d
@@ -0,0 +1,66 @@
+BMSmain1.1.o: ../BMSmain21/BMSmain1.1.c C:/WinAVR/avr/include/avr/io.h \
+  C:/WinAVR/avr/include/avr/sfr_defs.h C:/WinAVR/avr/include/inttypes.h \
+  C:/WinAVR/avr/include/stdint.h C:/WinAVR/avr/include/avr/iocan128.h \
+  C:/WinAVR/avr/include/avr/iocanxx.h \
+  C:/WinAVR/avr/include/avr/portpins.h \
+  C:/WinAVR/avr/include/avr/version.h C:/WinAVR/avr/include/util/delay.h \
+  C:/WinAVR/avr/include/avr/sleep.h ../BMSmain21/CellAnalysis.h \
+  ../BMSmain21/RTC_internal.h ../BMSmain21/uart_lib.h \
+  ../BMSmain21/uart_drv.h ../BMSmain21/config.h ../BMSmain21/compiler.h \
+  C:/WinAVR/avr/include/avr/interrupt.h ../BMSmain21/at90can_drv.h \
+  ../BMSmain21/dvk90can1_board.h \
+  C:/WinAVR/lib/gcc/avr/3.4.5/include/stdarg.h ../BMSmain21/can_drv.h \
+  ../BMSmain21/can_lib.h ../BMSmain21/F_RAM.h ../BMSmain21/Frontend.h \
+  ../BMSmain21/CAN_transmit.h ../BMSmain21/BMS_Config.h
+
+C:/WinAVR/avr/include/avr/io.h:
+
+C:/WinAVR/avr/include/avr/sfr_defs.h:
+
+C:/WinAVR/avr/include/inttypes.h:
+
+C:/WinAVR/avr/include/stdint.h:
+
+C:/WinAVR/avr/include/avr/iocan128.h:
+
+C:/WinAVR/avr/include/avr/iocanxx.h:
+
+C:/WinAVR/avr/include/avr/portpins.h:
+
+C:/WinAVR/avr/include/avr/version.h:
+
+C:/WinAVR/avr/include/util/delay.h:
+
+C:/WinAVR/avr/include/avr/sleep.h:
+
+../BMSmain21/CellAnalysis.h:
+
+../BMSmain21/RTC_internal.h:
+
+../BMSmain21/uart_lib.h:
+
+../BMSmain21/uart_drv.h:
+
+../BMSmain21/config.h:
+
+../BMSmain21/compiler.h:
+
+C:/WinAVR/avr/include/avr/interrupt.h:
+
+../BMSmain21/at90can_drv.h:
+
+../BMSmain21/dvk90can1_board.h:
+
+C:/WinAVR/lib/gcc/avr/3.4.5/include/stdarg.h:
+
+../BMSmain21/can_drv.h:
+
+../BMSmain21/can_lib.h:
+
+../BMSmain21/F_RAM.h:
+
+../BMSmain21/Frontend.h:
+
+../BMSmain21/CAN_transmit.h:
+
+../BMSmain21/BMS_Config.h:
---
+++ Appendix/Software/BatteryManagementSystem/bmsmain21/default/dep/RTC_internal.o.d
@@ -0,0 +1,57 @@
+RTC_internal.o: ../BMSmain21/RTC_internal.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iocan128.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iocanxx.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  ../BMSmain21/RTC_internal.h ../BMSmain21/uart_lib.h \
+  ../BMSmain21/uart_drv.h ../BMSmain21/config.h ../BMSmain21/compiler.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  ../BMSmain21/at90can_drv.h ../BMSmain21/dvk90can1_board.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iocan128.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iocanxx.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../BMSmain21/RTC_internal.h:
+
+../BMSmain21/uart_lib.h:
+
+../BMSmain21/uart_drv.h:
+
+../BMSmain21/config.h:
+
+../BMSmain21/compiler.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+../BMSmain21/at90can_drv.h:
+
+../BMSmain21/dvk90can1_board.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
